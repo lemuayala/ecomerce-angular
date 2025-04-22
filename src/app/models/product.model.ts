4d38b1114799bed57a553ef8ea137228293fc12f
lemuayala <lemuayala@gmail.com>
2025-04-22 20:47:26 -0300

    Se añade product.model

---
+++ src/app/models/product.model.ts
@@ -0,0 +1,10 @@
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags?: string[];
+  metadata?: { [key: string]: string };
+  discount?: number; // Para manejar descuentos
+}
